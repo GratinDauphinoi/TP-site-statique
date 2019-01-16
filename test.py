6c1cc4081165f10a9be3ea70c01660b8b198b385
Eddy <Eddy.chabrely@ynov.com>
2019-01-16 11:12:07 +0100

    argument avancement

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", help="Le chemin du dossier de fichiers source (contenant les fichiers markdown)")
-parser.add_argument("-o", help="le chemin du dossier où seront mis les fichiers générés pour le site statique")
+parser.add_argument("-i", help="Le chemin du dossier de fichiers source (contenant les fichiers markdown)", action="store_true")
+parser.add_argument("-o", help="le chemin du dossier où seront mis les fichiers générés pour le site statique", action="store_true")
 args = parser.parse_args()
-if args.verbosity:
-    print("verbosity turned on")
+
+if args.i:
+    print("chemin fichier source ")
+if args.o:
+    print("chemin fichier générés ")
