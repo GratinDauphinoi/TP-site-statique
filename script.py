81d645a68729fa2f337bbda9a454b7f7de5f7dac
Eddy <Eddy.chabrely@ynov.com>
2019-01-16 11:58:37 +0100

    remplacement = ok, bug = ok aussi

--- script.py
+++ script.py
@@ -5,14 +5,19 @@ markdown = fichier_markdown.read()
 print (markdown)
 
 fichier_html = open("html.txt", "w")
-fichier_html.write(markdown)
-
-lignes = fichier_markdown.readlines()
-for ligne in lignes:
-    Remplacement_h1 = ligne.replace("#", "<h1>")
-    fichier_html.write(Remplacement_h1)
 
 
+Remplacement_h1 = markdown.replace("#", "<h1>")
+fichier_html.write(Remplacement_h1)
+
+Remplacement_h2 = markdown.replace("##", "<h2>")
+fichier_html.write(Remplacement_h2)
+
+Remplacement_h3 = markdown.replace("##", "<h3>")
+fichier_html.write(Remplacement_h3)
+
+Remplacement_important = markdown.replace("*", "<em>")
+fichier_html.write(Remplacement_important)
 
 fichier_markdown.close()
 fichier_html.close()
