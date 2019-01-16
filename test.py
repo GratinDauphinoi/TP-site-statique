1acbfaf3f36f7e09bd35de72cd95d055b09c578c
Eddy <Eddy.chabrely@ynov.com>
2019-01-16 11:18:50 +0100

    + argument -t

--- test.py
+++ test.py
@@ -3,9 +3,12 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", help="Le chemin du dossier de fichiers source (contenant les fichiers markdown)", action="store_true")
 parser.add_argument("-o", help="le chemin du dossier où seront mis les fichiers générés pour le site statique", action="store_true")
+parser.add_argument("-t", help="le dossier contenant des modèles de pages web à compléter", action="store_true")
 args = parser.parse_args()
 
 if args.i:
-    print("chemin fichier source ")
+    print("https://github.com/GratinDauphinoi/TP-site-statique/blob/master/markdown.txt")
 if args.o:
-    print("chemin fichier générés ")
+    print("https://github.com/GratinDauphinoi/TP-site-statique/blob/master/html.txt")
+if args.t:
+    print("dossier modèles")
