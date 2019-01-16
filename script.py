5e25f11468e4598d10b0c46df2db7f0b9b876eaa
Eddy <Eddy.chabrely@ynov.com>
2019-01-16 11:46:11 +0100

    remplacement essais

--- script.py
+++ script.py
@@ -7,12 +7,10 @@ print (markdown)
 fichier_html = open("html.txt", "w")
 fichier_html.write(markdown)
 
-
-
-
-
-
-
+lignes = fichier_markdown.readlines()
+for ligne in lignes:
+    Remplacement_h1 = ligne.replace("#", "<h1>")
+    fichier_html.write(Remplacement_h1)
 
 
 
