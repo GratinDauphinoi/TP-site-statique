5a22b542b1e14f5ad50f5b1436c9ff5085237627
Eddy <Eddy.chabrely@ynov.com>
2019-01-26 18:22:33 +0100

    5635

--- test2.py
+++ test2.py
@@ -1,3 +1,5 @@
+import re 
+
 fichier_markdown = open("markdown.md", "r")
 markdown = fichier_markdown.read()
 #print (markdown)
@@ -16,15 +18,23 @@ print(valeurs)
 
 Remplacement = markdown.replace(str(cle),str(valeurs))
 fichier_html.write(Remplacement)
-'''
+
+
 Remplacement = markdown.replace("###", "<h3>").replace("##", "<h2>").replace("#", "<h1>")
-
-
-markdown.replace("*", "<em>")
 
 fichier_html.write(Remplacement)
 
+Remplacement = markdown.replace("*", "<em>")
 
+fichier_html.write(Remplacement)
+''' 
+
+
+
+
+oui = re.sub(r'(:[a-z]+)', r'<em>\1</em>', markdown)
+
+fichier_html.write(oui)
 
 fichier_markdown.close()
 fichier_html.close()
