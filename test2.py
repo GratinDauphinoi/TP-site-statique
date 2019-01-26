c882504558b8c00b1802a6123d3b3808b8584dbc
Eddy <Eddy.chabrely@ynov.com>
2019-01-26 18:39:25 +0100

    yes

--- test2.py
+++ test2.py
@@ -6,35 +6,40 @@ markdown = fichier_markdown.read()
 
 fichier_html = open("html.html", "w")
 
-'''
-liste = {"#" : "<h1>", "##" : "<h2>", "###" : "<h3>", "*" : "<em>"}
+# liste = {"#" : "<h1>", "##" : "<h2>", "###" : "<h3>", "*" : "<em>"}
 
-cle = liste.keys()
-print(cle)
+# cle = liste.keys()
+# print(cle)
 
-valeurs = liste.values()
-print(valeurs)
-
-
-Remplacement = markdown.replace(str(cle),str(valeurs))
-fichier_html.write(Remplacement)
+# valeurs = liste.values()
+# print(valeurs)
 
 
-Remplacement = markdown.replace("###", "<h3>").replace("##", "<h2>").replace("#", "<h1>")
+# Remplacement = markdown.replace(str(cle),str(valeurs))
+# fichier_html.write(Remplacement)
 
-fichier_html.write(Remplacement)
-
-Remplacement = markdown.replace("*", "<em>")
-
-fichier_html.write(Remplacement)
-''' 
+#oui = re.sub(r'*', r'<em>\1</em>', markdown)
+#fichier_html.write(oui)
 
 
 
 
-oui = re.sub(r'(:[a-z]+)', r'<em>\1</em>', markdown)
 
-fichier_html.write(oui)
+a = markdown.replace("###", "<h3>").replace("##", "<h2>").replace("#", "<h1>")
+
+a = a.replace("*", "<em>")
+
+
+print(a)
+
+fichier_html.write(a)
+
+
+
+
+
+
+
 
 fichier_markdown.close()
 fichier_html.close()
