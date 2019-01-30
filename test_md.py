017cbc39d5ebb069cdaee0ce8f5e94683808abe5
Eddy <Eddy.chabrely@ynov.com>
2019-01-30 10:09:26 +0100

    euh ?

---
+++ test_md.py
@@ -0,0 +1,12 @@
+import markdown
+import codecs
+
+input_file = codecs.open("markdown.md", mode="r", encoding="utf-8")
+text = input_file.read()
+html = markdown.markdown(text)
+
+output_file = codecs.open("html.html", "w",
+                          encoding="utf-8",
+                          errors="xmlcharrefreplace"
+)
+output_file.write(html)
